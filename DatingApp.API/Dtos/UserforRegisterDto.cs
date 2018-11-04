7c483c9c3dc64c6b4f97b3b66d7e3047a705742d
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-11-04 22:55:01 +0000

    JWT Login Tokens

--- DatingApp.API/Dtos/UserforRegisterDto.cs
+++ DatingApp.API/Dtos/UserforRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserforRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(16, MinimumLength=6, ErrorMessage="You must specify a password between 6 and 16 characters.")]
         public string Password { get; set; }
     }
 }
