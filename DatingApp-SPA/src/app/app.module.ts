38e0bc208aba5de256ed7fc19f49148cba622e2f
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-11-06 20:48:34 +0000

    Login and Logout

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './_services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
@@ -18,7 +19,9 @@ import {FormsModule} from '@angular/forms';
       HttpClientModule,
       FormsModule
    ],
-   providers: [],
+   providers: [
+       AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
