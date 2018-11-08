cc319fa608cd6d5ac1bdd8169221eb170e10a07e
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-11-08 20:54:17 +0000

    Protecting multiple routes with a single route guard using dummy routes

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -6,12 +6,18 @@ import { HomeComponent } from './home/home.component';
 import { MemberListComponent } from './member-list/member-list.component';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent},
-    { path:   'members', component: MemberListComponent, canActivate: [AuthGuard]},
 
+    { path: '', component: HomeComponent},
+{
+path: '',
+runGuardsAndResolvers: 'always',
+canActivate: [AuthGuard],
+children: [
+    { path:   'members', component: MemberListComponent},
     { path:   'messages', component: MessagesComponent},
-
     { path:   'lists', component: ListsComponent},
+]
+},
 
-    { path: '**', redirectTo: 'home', pathMatch: 'full'},
+    { path: '**', redirectTo: '', pathMatch: 'full'},
 ];
