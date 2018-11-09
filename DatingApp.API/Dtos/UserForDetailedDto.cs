e36efadc556f2140a0b953d9804e2846ff40c194
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-11-09 21:38:11 +0000

    AutoMapper

--- DatingApp.API/Dtos/UserForDetailedDto.cs
+++ DatingApp.API/Dtos/UserForDetailedDto.cs
@@ -22,7 +22,7 @@ namespace DatingApp.API.Dtos
 
         public string PhotoUrl { get; set; }
 
-        public ICollection<Photo> Photos { get; set; }
+        public ICollection<PhotosForDetailedDto> Photos { get; set; }
 
     }
 }
