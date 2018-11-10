ecf508680711f722831a6a7bd278dd6acd0b42b1
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-11-10 22:48:16 +0000

    Improved user detailed page

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberCardDetailsComponent } from './members/member-card-details/member-card-details.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { UserService } from './_services/user.service';
@@ -8,7 +9,7 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
-import { BsDropdownModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 
@@ -35,13 +36,15 @@ export function tokenGetter() {
       MemberListComponent,
       ListsComponent,
       MessagesComponent,
-      MemberCardDetailsComponent
+      MemberCardDetailsComponent,
+      MemberDetailComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BsDropdownModule.forRoot(),
+      TabsModule.forRoot(),
       RouterModule.forRoot(appRoutes),
       JwtModule.forRoot({
           config: {
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,3 +1,4 @@
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { AuthGuard } from './_guards/auth.guard';
 import {Routes} from '@angular/router';
 import { ListsComponent } from './lists/lists.component';
@@ -14,6 +15,7 @@ runGuardsAndResolvers: 'always',
 canActivate: [AuthGuard],
 children: [
     { path:   'members', component: MemberListComponent},
+    { path:   'members/:id', component: MemberDetailComponent},
     { path:   'messages', component: MessagesComponent},
     { path:   'lists', component: ListsComponent},
 ]
